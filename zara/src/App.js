0a0fe35b6b1a067c7682bd394f82232840bd14fa
itsvickyanand <vickyanandtoprock@gmail.com>
2022-12-14 20:02:14 +0530

    day_2

--- zara/src/App.js
+++ zara/src/App.js
@@ -1,17 +1,18 @@
- 
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import AllRoutes from "./pages/AllRoutes";
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import AllRoutes from './pages/AllRoutes'
+import Nav from './components/Navbar_Vicky/Nav.jsx'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Navbar />
-     <AllRoutes /> 
+      <AllRoutes />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
