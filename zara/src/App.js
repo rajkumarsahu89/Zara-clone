28eb134f8169095bad622867d31280c41e7699d9
itsvickyanand <vickyanandtoprock@gmail.com>
2022-12-14 20:39:20 +0530

    2

--- zara/src/App.js
+++ zara/src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AllRoutes from './pages/AllRoutes'
 import Nav from './components/Navbar_Vicky/Nav.jsx'
+import Mens from './components/Mens_Vicky/Mens'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Mens />
       <Navbar />
       <AllRoutes />
       <Footer />
