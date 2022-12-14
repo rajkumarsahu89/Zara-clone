cd4383ff7f383ea7f2b8f66f01de65a35fb85b16
rajkumarsahu89 <sahukumarraj89@gmail.com>
2022-12-14 13:56:24 +0530

    zara

--- zara/src/index.js
+++ zara/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+import { store } from "./store/store";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store} >
+   <ChakraProvider >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    </ChakraProvider> 
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
