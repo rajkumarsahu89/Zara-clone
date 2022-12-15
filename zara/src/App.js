acf10280c2c4c19ad8151c8677c73148b733b02e
sristy verma <sristyel2016@gmail.com>
2022-12-15 12:21:10 +0530

    woman slider page completed

--- zara/src/App.js
+++ zara/src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import AllRoutes from "./pages/AllRoutes";
+// import Slider from './components/Slider'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
      <AllRoutes /> 
+    
       <Footer />
+   
     </div>
   );
 }
