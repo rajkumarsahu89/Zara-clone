547cc66810f3fbddec5ce78be226ce880428fedd
itsvickyanand <vickyanandtoprock@gmail.com>
2022-12-15 16:33:12 +0530

    day3

--- zara/src/components/Mens_Vicky/Mens.jsx
+++ zara/src/components/Mens_Vicky/Mens.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Mens1 from './Mens1'
 import Mens2 from './Mens2'
+import Mens3 from './Mens3'
 import Mens4 from './Mens4'
+import Mens5 from './Mens5'
+import Mens6 from './Mens6'
 
 function Mens() {
   return (
     <div style={{ marginTop: '-100px' }}>
       <Mens1 />
       <Mens2 />
+      <Mens3 />
       <Mens4 />
+      <Mens5 />
+      <Mens6 />
     </div>
   )
 }
