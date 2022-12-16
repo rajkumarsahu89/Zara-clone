6b83a98509e7e947d250c9b43c703e6b154969ae
itsvickyanand <vickyanandtoprock@gmail.com>
2022-12-17 00:29:31 +0530

    3

---
+++ zara/src/Components/comp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function comp() {
+  return <div></div>
+}
+
+export default comp
---
+++ zara/src/Pages/pages.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function pages() {
+  return <div></div>
+}
+
+export default pages
