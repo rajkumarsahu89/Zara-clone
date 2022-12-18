3a1176ce7ed53ad5fbf6076fa5226c496fe74364
codersudhir <nandanesudhir1@gmail.com>
2022-12-18 19:40:54 +0530

    responsive

--- zara/src/Pages/Register/Register.jsx
+++ zara/src/Pages/Register/Register.jsx
@@ -1,10 +1,11 @@
-import { useToast ,Input} from '@chakra-ui/react'
+import { useToast ,Input,Center,Grid,GridItem} from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { authRegister } from '../../Redux/auth/actions'
 import "./Register.css"
+import "../Login/Login.css"
 
 const Register = () => {
     const [data, setData] = useState({
@@ -57,41 +58,42 @@ const Register = () => {
 
     return (
         <>
-            <div className='mains'>
+          <Center width="80vw">
+            <Grid className='main'>
+
+                <GridItem className='block1'  width="100%">
                 <div className='detail' >
                     <h2>PERSONAL DETAILS</h2>
                 </div>
                 <div className='radio'>
                     <div>
-                        <Input style={{ fontSize: "10px" }} type="radio" name="" id="" onChnage={handleInput} />
+                        <input style={{ fontSize: "10px" }} type="radio" name="" id="" onChnage={handleInput} />
                         <label style={{ marginLeft: "10px", fontSize: "10px" }} htmlFor="">PERSONAL</label>
                     </div>
                     <div>
-                        <Input style={{ marginLeft: "20px", fontSize: "10px" }} type="radio" name="" id="" onChnage={handleInput} />
+                        <input style={{ marginLeft: "20px", fontSize: "10px" }} type="radio" name="" id="" onChnage={handleInput} />
                         <label style={{ marginLeft: "10px", fontSize: "10px", marginTop: '10px' }} htmlFor="">COMPANY</label>
                     </div>
                 </div>
+
                 <div>
                     <input style={{ marginTop: '3em' }} className='inp' type="text" name='email' placeholder='E-MAIL' value={data.email} onChange={handleInput} />
                 </div>
+
                 <div>
                     <input style={{ marginRight: "3em" }} className='inp' type="text" name='password' placeholder='PASSWORD' value={data.password} onChange={handleInput} />
-                    <input className='inp' type="text" name='resetPassword' placeholder='REPEAT PASSWORD' value={data.resetPassword} onChange={handleInput} />
                 </div>
                 <div>
-                    <input style={{ marginRight: "3em" }} className='inp' type="text" name='name' placeholder='NAME' value={data.name} onChange={handleInput} />
-                    <input className='inp' type="text" name='pinCode' placeholder='PINCODE' value={data.pinCode} onChange={handleInput} />
+                <input style={{ marginRight: "3em",marginTop:"20px" }} className='inp' type="text" name='name' placeholder='NAME' value={data.name} onChange={handleInput} />
                 </div>
                 <div>
-                    <input style={{ marginRight: "3em" }} className='inp' type="text" name='address' placeholder='ADDRESS' value={data.address} onChange={handleInput} />
-                    <input className='inp' type="text" name='optional' placeholder='OPTIONAL' value={data.optional} onChange={handleInput} />
+                <input style={{ marginRight: "3em",marginTop:"25px" }} className='inp' type="text" name='address' placeholder='ADDRESS' value={data.address} onChange={handleInput} />
                 </div>
                 <div>
-                    <input style={{ marginRight: "3em" }} className='inp' type="text" name='locality' placeholder='LOCALITY' value={data.locality} onChange={handleInput} />
-                    <input className='inp' type="text" name='city' placeholder='CITY' value={data.city} onChange={handleInput} />
+                <input style={{ marginRight: "3em",marginTop:"25px" }} className='inp' type="text" name='locality' placeholder='LOCALITY' value={data.locality} onChange={handleInput} />
                 </div>
                 <div className='state'>
-                    <select className='inp' onChange={handleInput} name='state'>
+                    <select className='inp' onChange={handleInput} name='state' style={{width:"90%",marginTop:"15px"}}>
                         <option value="">STATE</option>
                         <option value="Bihar">Bihar</option>
                         <option value="Mumbai">Maharashtra</option>
@@ -108,7 +110,7 @@ const Register = () => {
                         <option value="Madhya Pradesh">Madhya Pradesh</option>
                         <option value="Jammu Kashmir">Jammu Kashmir</option>
                     </select>
-                    <div className='reg'>
+                    <div className='reg' style={{marginTop:"15px"}}>
                         <p>REGION</p>
                         <p>India</p>
                     </div>
@@ -128,11 +130,39 @@ const Register = () => {
                         <label className='lab' htmlFor="">I ACCEPT THE <u> PRIVACY STATEMENT</u></label>
                     </div>
                 </div>
-                <button className='button' onClick={() => dispatch(authRegister(data))}>CREATE ACCOUNT</button>
-            </div>
-            <div className=' bottom'>
-                <div>
-                    <h3>HELP</h3>
+                
+                
+                    
+                </GridItem>
+                <GridItem className='block2' width={"100%"} >
+                    <div>
+                    <input  style={{marginTop:"180px",width:"100%"}} className='inp' type="text" name='resetPassword' placeholder='REPEAT PASSWORD' value={data.resetPassword} onChange={handleInput} />
+                    </div>
+
+                  <div>
+                  <input style={{width:"100%"}} className='inp' type="text" name='pinCode' placeholder='PINCODE' value={data.pinCode} onChange={handleInput} />
+                  </div>
+                  <div>
+                  <input style={{width:"100%"}} className='inp'  type="text" name='optional' placeholder='OPTIONAL' value={data.optional} onChange={handleInput} />
+                  </div>
+                  <div>    
+                    <input style={{width:"100%"}} className='inp' type="text" name='city' placeholder='CITY' value={data.city} onChange={handleInput} />
+                </div>
+               
+                </GridItem>
+                
+            </Grid>
+            </Center>
+            <Center>
+            <button style={{width:"30%",marginLeft:"0%"}} className='button' onClick={() => dispatch(authRegister(data))}>CREATE ACCOUNT</button>
+            </Center>
+           
+            
+            <Center w="100vw">
+            <div className='bottom'>
+                <Grid className='projects_grid'>  
+                <GridItem >
+                    <h3> HELP</h3>
                     <p>SHOP AT ZARA.COM</p>
                     <p>PRODUCT</p>
                     <p>GIFT CARD</p>
@@ -142,8 +172,8 @@ const Register = () => {
                     <p>SHOPS AND COMPANY</p>
                     <p>CLOTHES COLLECTION PROGRAMME</p>
                     <p>MY ACCOUNT</p>
-                </div>
-                <div>
+                </GridItem>
+                <GridItem>
                     <h3>FOLLOW US</h3>
                     <p>NEWSLETTER</p>
                     <p>INSTAGRAM</p>
@@ -151,32 +181,39 @@ const Register = () => {
                     <p>TWITTER</p>
                     <p>PINTREST</p>
                     <p>YOUTUBE</p>
-                </div>
-                <div>
+                </GridItem>
+                <GridItem>
                     <h3>COMPANY</h3>
                     <p>ABOUT US</p>
                     <p>JOIN LIFE</p>
                     <p>OFFICES</p>
                     <p>STORIES</p>
                     <p>WORK WITH US</p>
-                </div>
-                <div>
+                </GridItem>
+                <GridItem>
                     <h3>POLICIES</h3>
                     <p>PRIVACY POLICY</p>
                     <p>PURCHASING CONDITIONS</p>
                     <p>GIFT CARD CONDITIONS</p>
                     <p>COOKIES SETTINGS</p>
-                </div>
+                </GridItem>
+                </Grid>
             </div>
-            <div className='line'>
+            
+            </Center >
+            <Center width="100vw">
+                <Grid className='main' >
+            <GridItem className='line'>
                 <p>INDIA</p>
                 <p>ENGLISH  © ALL RIGHTS RESERVED</p>
-            </div>
-            <div className='bot2'>
+            </GridItem>
+            <GridItem className='bot2'>
                 <p>NAME AND ADDRESS OF THE MANUFACTURER:</p>
                 <p>INDUSTRIA DE DISEÑO TEXTIL, S.A. (INDITEX, S.A.)</p>
                 <p>AVENIDA DE LA DIPUTACIÓN, EDIFICIO INDITEX, 15143, ARTEIXO (A CORUÑA), SPAIN</p>
-            </div>
+            </GridItem>
+            </Grid>
+            </Center>
 
         </>
     )
